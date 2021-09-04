260b7fb9f95164f3a78acd12869dbdb86ba99e96
merwhite11 <meredith.white91@gmail.com>
2021-09-04 11:52:18 -0700

    Complete Bare Minimum Requirements

--- spec/bouncyBlinkyDancerSpec.js
+++ spec/bouncyBlinkyDancerSpec.js
@@ -8,9 +8,9 @@ describe('bouncyBlinkyDancer', function() {
     bouncyBlinkyDancer = new makeBouncyBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
-  // it('should have a jQuery $node object', function() {
-  //   expect(bouncyBlinkyDancer.$node).to.be.an.instanceof(jQuery);
-  // });
+  it('should have a jQuery $node object', function() {
+    expect(bouncyBlinkyDancer.$node).to.be.an.instanceof(jQuery);
+  });
 
   // it('should have a step function that makes its node blink', function() {
   //   sinon.spy(bouncyBlinkyDancer.$node, 'toggle');
